b5745a39c785cefe26b8d1f15594d4c4ca00313f
Pirasak S. <guzzguzzguzz96@gmail.com>
2026-01-26 17:56:50 +0700

    delete console log
    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,4 @@ const nextConfig = {
   },
 };
 
-console.log("NEXT CONFIG DEBUG", {
-  output: nextConfig.output,
-  NEXT_OUTPUT: process.env.NEXT_OUTPUT,
-  OUTPUT: process.env.OUTPUT,
-  VERCEL_ENV: process.env.VERCEL_ENV,
-});
-
 export default nextConfig;
