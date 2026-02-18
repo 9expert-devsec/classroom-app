2f3da6073b7a0236b8fd5ef6e0b5abc4ac3fbb9c
NU-YaniP <yanipong.nu1005@gmail.com>
2026-02-18 14:01:03 +0700

    edit layout checkin page

--- src/app/classroom/checkin/layout.jsx
+++ src/app/classroom/checkin/layout.jsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
+import Image from "next/image";
 
 const lineSeedSansTH = localFont({
   src: [
@@ -35,25 +36,50 @@ export default function CheckinLayout({ children }) {
     <div
       className={`${lineSeedSansTH.className} h-screen bg-[#F8FAFD] text-front-text`}
     >
-      <div className="mx-auto flex h-screen max-w-4xl flex-col px-4 py-6">
-        <header className="mb-6 shrink-0 flex flex-row items-center gap-3">
-          <Link
+      <div className="mx-auto flex h-dvh max-w-4xl flex-col px-4 py-6">
+        <div className="flex flex-col items-center justify-center text-center my-4">
+          <Image
+            src="/logo-9expert-app.png"
+            alt="9Expert"
+            width={90}
+            height={90}
+            priority
+          />
+
+          <header className="mt-4 flex flex-col items-center gap-1">
+            <h1 className="sm:text-4xl lg:text-2xl font-semibold">
+              9Expert Register
+            </h1>
+            <p className="sm:text-lg lg:text-sm text-front-textMuted">
+              กรุณาทำตามขั้นตอนเพื่อเช็คอินเข้าห้องเรียน
+            </p>
+          </header>
+        </div>
+        {/* <Image
+          src="/logo-9expert-app.png"
+          width={80}
+          height={80}
+          priority
+        /> */}
+
+        {/* <header className="mb-6 shrink-0 flex flex-row items-center gap-3"> */}
+        {/* <Link
               href="/classroom"
               className="inline-flex h-9 w-9 items-center justify-center rounded-full border border-admin-border bg-white text-admin-text hover:bg-admin-surfaceMuted"
               aria-label="ย้อนกลับ"
             >
               <ChevronLeft className="h-4 w-4" />
-            </Link>
+            </Link> */}
 
-          <div>
-            <h1 className="text-2xl font-semibold">
+        {/* <div className="flex flex-col gap-1">
+            <h1 className=" sm:text-4xl lg:text-2xl font-semibold">
               9Expert Classroom Check-in
             </h1>
-            <p className="text-sm text-front-textMuted">
+            <p className="sm:text-lg lg:text-sm text-front-textMuted">
               กรุณาทำตามขั้นตอนเพื่อเช็คอินเข้าห้องเรียน
             </p>
           </div>
-        </header>
+        </header> */}
 
         {/* ✅ เลื่อนเฉพาะ main */}
         <main className="min-h-0 flex-1 overflow-hidden rounded-3xl shadow-md">
@@ -62,7 +88,7 @@ export default function CheckinLayout({ children }) {
           </div>
         </main>
 
-        <footer className="mt-4 shrink-0 text-center text-xs text-front-textMuted">
+        <footer className="mt-4 shrink-0 text-center sm:text-base lg:text-xs text-front-textMuted">
           © 9Expert Training
         </footer>
       </div>
